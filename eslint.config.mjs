aa9f42ccf93e81ca9786531284eeacbc80c78d1d
zulfa rifandi <zulfarifandias@gmail.com>
2024-06-29 22:22:56 +0700

    solve home

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,23 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-import prettier from
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended';
+import prettier from 'eslint-config-prettier';
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: globals.browser,
+    },
+  },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  tseslint.configs.recommended,
   pluginReactConfig,
+  prettier,
 ];
